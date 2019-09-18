08c33789f1d54e2360aea220fe5926ff84da57e8
Martin <martinwang7963@gmail.com>
2019-09-18 14:07:51 +0800

    First Test

--- MartinBot.Net.Test/CrawlerServiceTest.cs
+++ MartinBot.Net.Test/CrawlerServiceTest.cs
@@ -1,13 +1,30 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MartinBot.Net.Config;
+using MartinBot.Net.Services;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
 using NUnit.Framework;
 
-namespace Tests {
-    public class Tests {
+namespace MartinBot.Net.Test {
+    [TestFixture]
+    public class CrawlerServiceTest {
+
         [SetUp]
         public void Setup () { }
 
         [Test]
-        public void CanCrawlerDownloadLineSticker () {
-            Assert.Pass ();
+        public void CanGetLineStickerUrlsGreaterThanZero () {
+            var logger = new Mock<ILogger<CrawlerService>> ().Object;
+            var config = new Mock<IOptions<CrawlerConfig>> ().Object;
+
+            var service = new CrawlerService (config, logger);
+
+            var url = "";
+            var result = service.GetLineStickerUrlsAsync (url).Result;
+            Assert.Greater (0, result.Count);
         }
+
     }
 }
