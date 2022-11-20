09d6ef931ba7ad0f7a3b026aa093219c3fde0054
MartinWang <martinwang7963@gmail.com>
2022-11-20 22:50:23 +0800

    add stickerSet check

--- LineStickerToTGBot.Net/Services/LineStickerConvertService.cs
+++ LineStickerToTGBot.Net/Services/LineStickerConvertService.cs
@@ -63,7 +63,9 @@ namespace LineStickerToTGBotAPI.Services
             var stickerName = $"botLineStickerToTg_by_{botInfo.Username}";
             //var fileBytes = await IOFile.ReadAllBytesAsync(url);
             int firstEmojiUnicode = 0x1F601;
-            var emojiString = char.ConvertFromUtf32(firstEmojiUnicode);
+            //var emojiString = char.ConvertFromUtf32(firstEmojiUnicode);
+
+
 
             //var process = new Process
             //{
@@ -185,10 +187,11 @@ namespace LineStickerToTGBotAPI.Services
 
         }
 
-        private async Task<List<Telegram.Bot.Types.File>> UploadedStaticStickerToTGSticker(long userId, string stickerName, string emojis, int stickerNumber)
+        private async Task<List<Telegram.Bot.Types.File>> UploadedStaticStickerToTGSticker(long userId, int emojiUnicode, int stickerNumber)
         {
             using (var hc = new HttpClient())
             {
+                var emojiString = char.ConvertFromUtf32(emojiUnicode);
                 Stream imgsZip = null;
                 string downloadUrl = string.Format(_botConfig.LineStaticStickerUrl, stickerNumber);
                 List<Telegram.Bot.Types.File> files = new List<Telegram.Bot.Types.File>();
@@ -234,11 +237,40 @@ namespace LineStickerToTGBotAPI.Services
             }
         }
 
-        private async Task AddConvertedVideoStickerToTGSticker(long userId, string stickerName, string emojis, int stickerNumber)
+        private async Task CheckAndSetStickerSetAsync(string stickerName, string title, long userId, string emojiString, ZipArchive zipFile, string ffmpegArg)
+        {
+            var stickerSet = await _botClient.GetStickerSetAsync(name: stickerName);
+            if (stickerSet == null)
+            {
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    var firstSticker = zipFile.Entries.FirstOrDefault(entry => Regex.IsMatch(entry.FullName, @"animation@2x\/\d+\@2x.png"));
+                    if (firstSticker != null)
+                    {
+                        var result = await Cli.Wrap(_ffmpegPathRootPath)
+                                     .WithArguments(ffmpegArg)
+                                     .WithStandardInputPipe(PipeSource.FromStream(firstSticker.Open()))
+                                     .WithStandardOutputPipe(PipeTarget.ToStream(ms))
+                                     .ExecuteAsync();
+                        ms.Seek(0, SeekOrigin.Begin);
+                        await _botClient.CreateNewVideoStickerSetAsync(
+                                            userId: userId,
+                                            name: stickerName,
+                                            title: title,
+                                            webmSticker: new Telegram.Bot.Types.InputFiles.InputFileStream(ms),
+                                            emojis: emojiString);
+                    }
+                }
+            }
+        }
+
+        private async Task AddConvertedVideoStickerToTGSticker(long userId, string title, string stickerName, int emojiUnicode, int stickerNumber)
         {
 
             using (var hc = new HttpClient())
             {
+                var emojiString = char.ConvertFromUtf32(emojiUnicode);
+                var ffmpegArg = @$"-i pipe:.png -vf scale=512:-1 -f webm pipe:1";
                 Stream imgsZip = null;
                 string downloadUrl = string.Format(_botConfig.LineAnimatedStickerUrl, stickerNumber);
                 try
@@ -252,7 +284,16 @@ namespace LineStickerToTGBotAPI.Services
                 }
                 using (ZipArchive zipFile = new ZipArchive(imgsZip))
                 {
-                    var ffmpegArg = @$"-i pipe:.png -vf scale=512:-1 -f webm pipe:1";
+                    try
+                    {
+                        await CheckAndSetStickerSetAsync(stickerName, title, userId, emojiString, zipFile, ffmpegArg);
+                    }
+                    catch (Exception ex)
+                    {
+                        _logger.LogError(ex.ToString());
+                        await CheckAndSetStickerSetAsync(stickerName, title, userId, emojiString, zipFile, ffmpegArg);
+                    }
+
                     foreach (var entry in zipFile.Entries)
                     {
                         if (Regex.IsMatch(entry.FullName, @"animation@2x\/\d+\@2x.png"))
@@ -262,7 +303,7 @@ namespace LineStickerToTGBotAPI.Services
                                 using (MemoryStream ms = new MemoryStream())
                                 {
 
-                                    var result = await Cli.Wrap(@"C:\ProgramData\chocolatey\bin\ffmpeg.exe")
+                                    var result = await Cli.Wrap(_ffmpegPathRootPath)
                                                  .WithArguments(ffmpegArg)
                                                  .WithStandardInputPipe(PipeSource.FromStream(entry.Open()))
                                                  .WithStandardOutputPipe(PipeTarget.ToStream(ms))
@@ -272,7 +313,7 @@ namespace LineStickerToTGBotAPI.Services
                                          userId: userId,
                                          name: stickerName,
                                          webmSticker: new Telegram.Bot.Types.InputFiles.InputFileStream(ms),
-                                         emojis: emojis);
+                                         emojis: emojiString);
                                 }
                             }
                             catch (Exception ex)
